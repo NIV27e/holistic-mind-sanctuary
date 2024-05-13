05ed303c7e25f2dfd17d0f9e342f0ec3bf63828e
Nivedh <nivnair21@gmail.com>
2024-05-13 22:53:41 +0100

    Added Services to Model.py

--- holistic_mind_sanctuary/sanctuary/models.py
+++ holistic_mind_sanctuary/sanctuary/models.py
@@ -8,4 +8,10 @@ class Zone(models.Model):
     description = models.TextField()
 
 
+class Service(models.Model):
+    zone = models.ForeignKey(Zone, related_name='services', on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+
 
